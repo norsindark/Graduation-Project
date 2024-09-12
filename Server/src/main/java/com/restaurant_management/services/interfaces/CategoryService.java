d0a5b1efcc3948cd9aadbb77bca7aad7ff63048c
Nguyen Tuan <norsinlight@gmail.com>
2024-09-12 23:39:55 +0700

    - Update Category API (12/09)

--- Server/src/main/java/com/restaurant_management/services/interfaces/CategoryService.java
+++ Server/src/main/java/com/restaurant_management/services/interfaces/CategoryService.java
@@ -4,12 +4,12 @@ import com.restaurant_management.dtos.CategoryDto;
 import com.restaurant_management.exceptions.DataExitsException;
 import com.restaurant_management.payloads.responses.ApiResponse;
 import com.restaurant_management.payloads.responses.CategoryResponse;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 
 public interface CategoryService {
 
-    Page<CategoryResponse> getAllCategories(int pageNo, int pageSize, String sortBy)
+    PagedModel<EntityModel<CategoryResponse>> getAllCategories(int pageNo, int pageSize, String sortBy)
             throws DataExitsException;
 
     ApiResponse addCategory(CategoryDto categoryDto) throws DataExitsException;
