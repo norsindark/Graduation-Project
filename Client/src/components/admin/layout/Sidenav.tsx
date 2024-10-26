8a7321272cf8cb4533e4a51adc169862fd588cb4
Lâm Văn Đức <124279537+DuckFunny2k@users.noreply.github.com>
2024-10-26 23:37:27 +0700

    handleViewAndDragData(70%)

--- Client/src/components/admin/layout/Sidenav.tsx
+++ Client/src/components/admin/layout/Sidenav.tsx
@@ -5,7 +5,7 @@ import { NavLink, useLocation } from 'react-router-dom';
 import { BiSolidDashboard } from 'react-icons/bi';
 import { CgMenuLeftAlt } from 'react-icons/cg';
 import { AiOutlineHome } from 'react-icons/ai';
-import { FaWarehouse } from 'react-icons/fa';
+import { FaReplyAll, FaWarehouse } from 'react-icons/fa';
 import { UsergroupAddOutlined } from '@ant-design/icons';
 import { FaCalendarCheck } from 'react-icons/fa';
 import { FaTags } from 'react-icons/fa';
@@ -146,6 +146,20 @@ function Sidenav({ color }: { color: string }) {
             <FaWarehouse />
           </span>
           <span className="label">Order</span>
+        </NavLink>
+      ),
+    },
+    {
+      key: '11',
+      label: (
+        <NavLink to="/review">
+          <span
+            className="icon"
+            style={{ background: page === 'review' ? color : '' }}
+          >
+            <FaReplyAll />
+          </span>
+          <span className="label">Review</span>
         </NavLink>
       ),
     },
