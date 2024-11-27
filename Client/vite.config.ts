0d27995d858c723bffe8f80cea35571c8f91a907
Lâm Văn Đức <124279537+DuckFunny2k@users.noreply.github.com>
2024-11-27 15:48:18 +0700

    Enhance Vite configuration and update component props
    - Updated Vite configuration to improve build settings, including external dependencies and chunk size limits.
    - Added `setActiveModal` prop to `Account` component for better modal management.
    - Integrated `setActiveModal` in `CheckoutPage` and `OrderSuccess` components to streamline modal interactions.
    - Updated `LayoutPublic` to include `editingAddressId` in context for improved state management.
    
    These changes aim to enhance the application's modularity and improve the user experience with modal handling.

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,18 +1,49 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     watch: {
-      usePolling: true,
+      usePolling: true, // Hữu ích khi chạy trong môi trường Docker hoặc hệ thống file không hỗ trợ theo dõi sự thay đổi file tự động.
     },
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 3000, // you can replace this port with any port
+    host: true, // Để container Docker hoặc máy chủ khác có thể truy cập
+    strictPort: true, // Đảm bảo dùng chính xác port đã cấu hình
+    port: 3000, // Thay đổi nếu cần port khác
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        'jquery', // Đánh dấu là external, sẽ không được bundle
+        'bootstrap',
+        'slick-carousel',
+        'isotope-layout',
+        'simplycountdown',
+        'venobox',
+        'wowjs',
+      ],
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Để lại các thư viện cần thiết
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1500, // Tăng giới hạn cảnh báo kích thước chunk lên 1000 kB
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Giúp dễ dàng import các tệp trong src (ví dụ: `import utils from '@/utils'`)
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/global.scss";`, // Tự động import SCSS toàn cục nếu bạn dùng SCSS
+      },
+    },
   },
   optimizeDeps: {
-    exclude: ['js-big-decimal'],
+    exclude: ['js-big-decimal'], // Loại trừ thư viện khỏi quá trình optimization
   },
+  base: './', // Đặt đường dẫn gốc, hữu ích nếu bạn deploy ứng dụng trong subfolder
 });
